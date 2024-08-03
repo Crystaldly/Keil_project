24f7e889bb559c166186fb818dcc21360c5f5117
Crystaldly <crystal_d1109@163.com>
2024-08-03 20:45:35 +0800

    修改、添加文档

---
+++ 1-1动态方法创建和删除任务/User/freertos_demo.c
@@ -0,0 +1,128 @@
+#include "freertos_demo.h"
+
+#include "main.h"
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+/******************************************************************************************************/
+/*FreeRTOS配置*/
+
+/* START_TASK 任务 配置
+ * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
+ */
+#define START_TASK_PRIO         1
+#define START_TASK_STACK_SIZE   128
+//动态任务参数配置
+TaskHandle_t    start_task_handler;
+void start_task( void * pvParameters );
+
+//静态任务参数配置
+TaskHandle_t start_static_task_handler;
+StaticTask_t start_task_tcb;
+StackType_t  start_task_stack[START_TASK_STACK_SIZE];
+
+/* TASK1 任务 配置
+ * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
+ */
+#define TASK1_PRIO         2
+#define TASK1_STACK_SIZE   128
+TaskHandle_t    task1_handler;
+void task1( void * pvParameters );
+
+/* TASK2 任务 配置
+ * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
+ */
+#define TASK2_PRIO         3
+#define TASK2_STACK_SIZE   128
+TaskHandle_t    task2_handler;
+void task2( void * pvParameters );
+
+/* TASK3 任务 配置
+ * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
+ */
+#define TASK3_PRIO         4
+#define TASK3_STACK_SIZE   128
+TaskHandle_t    task3_handler;
+void task3( void * pvParameters );
+/******************************************************************************************************/
+
+
+/**
+ * @brief       FreeRTOS例程入口函数
+ * @param       无
+ * @retval      无
+ */
+void freertos_demo(void)
+{    
+    xTaskCreate((TaskFunction_t         )   start_task,
+                (char *                 )   "start_task",
+                (configSTACK_DEPTH_TYPE )   START_TASK_STACK_SIZE,
+                (void *                 )   NULL,
+                (UBaseType_t            )   START_TASK_PRIO,
+                (TaskHandle_t *         )   &start_task_handler );
+
+    start_static_task_handler = xTaskCreateStatic( (TaskFunction_t )   start_task,
+                                                   (char *         )   "start_task", 
+                                                   (uint32_t       )   START_TASK_STACK_SIZE,
+                                                   (void *         )   NULL,
+                                                   (UBaseType_t    )   START_TASK_PRIO,
+                                                   (StackType_t *  )   start_task_stack,
+                                                   (StaticTask_t * )   &start_task_tcb );
+}
+
+void start_task( void * pvParameters )
+{
+    taskENTER_CRITICAL();               /* 进入临界区 */
+    xTaskCreate((TaskFunction_t         )   task1,
+                (char *                 )   "task1",
+                (configSTACK_DEPTH_TYPE )   TASK1_STACK_SIZE,
+                (void *                 )   NULL,
+                (UBaseType_t            )   TASK1_PRIO,
+                (TaskHandle_t *         )   &task1_handler );
+                
+    xTaskCreate((TaskFunction_t         )   task2,
+                (char *                 )   "task2",
+                (configSTACK_DEPTH_TYPE )   TASK2_STACK_SIZE,
+                (void *                 )   NULL,
+                (UBaseType_t            )   TASK2_PRIO,
+                (TaskHandle_t *         )   &task2_handler );
+                
+    xTaskCreate((TaskFunction_t         )   task3,
+                (char *                 )   "task3",
+                (configSTACK_DEPTH_TYPE )   TASK3_STACK_SIZE,
+                (void *                 )   NULL,
+                (UBaseType_t            )   TASK3_PRIO,
+                (TaskHandle_t *         )   &task3_handler );
+    vTaskDelete(NULL);
+    taskEXIT_CRITICAL();                /* 退出临界区 */
+}
+
+/* 任务一，实现LED0每500ms翻转一次 */
+void task1( void * pvParameters )
+{
+    while(1)
+    {
+        HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_4);
+        vTaskDelay(100);
+    }
+}
+
+/* 任务二，实现LED1每500ms翻转一次 */
+void task2( void * pvParameters )
+{
+    while(1)
+    {
+        HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_5);
+        vTaskDelay(100);
+    }
+}
+
+void task3( void * pvParameters )
+{
+    while(1)
+    {
+        HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_6);
+        vTaskDelay(10);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Keil_project
 keil工程
+# FreeRTOS移植成功及创建简单任务
+*2024.7.31*
+**移植步骤**
+1. 目录添加源码文件
+   1. 在工程中创建 “FreeRTOS” 文件夹，并且创建 “portable” 和“source” 两个空文件夹。
+   2. 拷贝FreeRTOS源码Source文件下7个.文件到新创建的 “source”文件夹下。
+   3. 拷贝FreeRTOS源码Portable文件夹下的 “Keil”、“RVDS”、 “MemMang”到新创建的 “portable”文件夹下。--其中MenMang可以只保留heap_4.c文件。
+   4. 拷贝FreeRTOS源码include文件夹到 “FreeRTOS”文件夹下。
+   5. FreeRTOSConfig.h 文件是 FreeRTOS 的工程配置文件，在源码“..\FreeRTOS\Demo”文件夹下面找到 “CORTEX_STM32F103_Keil” 这个文件夹下，找到 “FreeRTOSConfig.h”文件，然后拷贝到我们工程下的 “Core/Inc” 文件夹下即可。
+2. 工程中添加源码文件
+   1. 工程新建Group“FreeRTOS/Source”和“FreeRTOS/Portable”。。
+   2. 分别按照上述内容添加C文件。
+   3. 添加头文件，添加相应路径。
+3. 系统配置文件的修改
+   1. FreeRTOSConfig.h中添加如下3个配置：
+   `#define xPortPendSVHandler  PendSV_Handler`
+   `#define vPortSVCHandler     SVC_Handler` 
+   `#define INCLUDE_xTaskGetSchedulerState   1`
+   2. 修改stm32f1xx_it.c
+     1. 引入头文件
+      `#include "FreeRTOS.h"`
+      `#include "task.h"`--注意这两个头文件有先后次序
+     2. 注释头文件
+      将PendSV_Handler、SVC_Handler 两个头文件注释掉。
+     3. 添加SysTick时钟中断服务函数
+      `extern void xPortSysTickHandler(void);`
+      `if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED) `
+        `xPortSysTickHandler();`
+
+    
